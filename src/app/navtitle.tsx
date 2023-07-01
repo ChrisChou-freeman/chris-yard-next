9751a6c198076320aca4fbbfc275c4e6b9ecc4af
ChrisChou-freeman <ChrisChouFreeMan@hotmail.com>
2023-07-02 01:29:11 +0800

    update

--- src/app/navtitle.tsx
+++ src/app/navtitle.tsx
@@ -3,8 +3,8 @@ import { JSX } from "react"
 export function NavTitle(): JSX.Element {
   return (
     <nav className='flex justify-end w-full pr-10 gap-10 pt-5'>
-      <a href='/'>Home</a>
-      <a href='/'>Blog</a>
+      <a className='hover:underline' href='/'>Home</a>
+      <a className='hover:underline' href='/'>Blog</a>
     </nav>
   )
 }
